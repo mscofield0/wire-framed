bb5e2d5c8ee309028fd911327c6df077c30faa34
Michael Scofield <mscofield0@tutanota.com>
2023-05-16 01:56:26 +0200

    Add missing FromFrame for tuples

--- src/wire-framed-core/src/common_impls.rs
+++ src/wire-framed-core/src/common_impls.rs
@@ -372,3 +372,143 @@ where
 		self.0.size_hint() + self.1.size_hint() + self.2.size_hint() + self.3.size_hint() + self.4.size_hint() + self.5.size_hint() + self.6.size_hint() + self.7.size_hint()
 	}
 }
+
+impl<T1, T2> FromFrame for (T1, T2)
+where
+	T1: FromFrame,
+	T2: FromFrame,
+	<T1 as FromFrame>::Error: Into<std::io::Error>,
+	<T2 as FromFrame>::Error: Into<std::io::Error>,
+{
+	type Error = std::io::Error;
+
+	fn parse_frame(frame: &mut Bytes) -> Result<Self, Self::Error> {
+		Ok((T1::parse_frame(frame).map_err(Into::into)?, T2::parse_frame(frame).map_err(Into::into)?))
+	}
+}
+
+impl<T1, T2, T3> FromFrame for (T1, T2, T3)
+where
+	T1: FromFrame,
+	T2: FromFrame,
+	T3: FromFrame,
+	<T1 as FromFrame>::Error: Into<std::io::Error>,
+	<T2 as FromFrame>::Error: Into<std::io::Error>,
+	<T3 as FromFrame>::Error: Into<std::io::Error>,
+{
+	type Error = std::io::Error;
+
+	fn parse_frame(frame: &mut Bytes) -> Result<Self, Self::Error> {
+		Ok((T1::parse_frame(frame).map_err(Into::into)?, T2::parse_frame(frame).map_err(Into::into)?, T3::parse_frame(frame).map_err(Into::into)?))
+	}
+}
+
+impl<T1, T2, T3, T4> FromFrame for (T1, T2, T3, T4)
+where
+	T1: FromFrame,
+	T2: FromFrame,
+	T3: FromFrame,
+	T4: FromFrame,
+	<T1 as FromFrame>::Error: Into<std::io::Error>,
+	<T2 as FromFrame>::Error: Into<std::io::Error>,
+	<T3 as FromFrame>::Error: Into<std::io::Error>,
+	<T4 as FromFrame>::Error: Into<std::io::Error>,
+{
+	type Error = std::io::Error;
+
+	fn parse_frame(frame: &mut Bytes) -> Result<Self, Self::Error> {
+		Ok((T1::parse_frame(frame).map_err(Into::into)?, T2::parse_frame(frame).map_err(Into::into)?, T3::parse_frame(frame).map_err(Into::into)?, T4::parse_frame(frame).map_err(Into::into)?))
+	}
+}
+
+impl<T1, T2, T3, T4, T5> FromFrame for (T1, T2, T3, T4, T5)
+where
+	T1: FromFrame,
+	T2: FromFrame,
+	T3: FromFrame,
+	T4: FromFrame,
+	T5: FromFrame,
+	<T1 as FromFrame>::Error: Into<std::io::Error>,
+	<T2 as FromFrame>::Error: Into<std::io::Error>,
+	<T3 as FromFrame>::Error: Into<std::io::Error>,
+	<T4 as FromFrame>::Error: Into<std::io::Error>,
+	<T5 as FromFrame>::Error: Into<std::io::Error>,
+{
+	type Error = std::io::Error;
+
+	fn parse_frame(frame: &mut Bytes) -> Result<Self, Self::Error> {
+		Ok((T1::parse_frame(frame).map_err(Into::into)?, T2::parse_frame(frame).map_err(Into::into)?, T3::parse_frame(frame).map_err(Into::into)?, T4::parse_frame(frame).map_err(Into::into)?, T5::parse_frame(frame).map_err(Into::into)?))
+	}
+}
+
+impl<T1, T2, T3, T4, T5, T6> FromFrame for (T1, T2, T3, T4, T5, T6)
+where
+	T1: FromFrame,
+	T2: FromFrame,
+	T3: FromFrame,
+	T4: FromFrame,
+	T5: FromFrame,
+	T6: FromFrame,
+	<T1 as FromFrame>::Error: Into<std::io::Error>,
+	<T2 as FromFrame>::Error: Into<std::io::Error>,
+	<T3 as FromFrame>::Error: Into<std::io::Error>,
+	<T4 as FromFrame>::Error: Into<std::io::Error>,
+	<T5 as FromFrame>::Error: Into<std::io::Error>,
+	<T6 as FromFrame>::Error: Into<std::io::Error>,
+{
+	type Error = std::io::Error;
+
+	fn parse_frame(frame: &mut Bytes) -> Result<Self, Self::Error> {
+		Ok((T1::parse_frame(frame).map_err(Into::into)?, T2::parse_frame(frame).map_err(Into::into)?, T3::parse_frame(frame).map_err(Into::into)?, T4::parse_frame(frame).map_err(Into::into)?, T5::parse_frame(frame).map_err(Into::into)?, T6::parse_frame(frame).map_err(Into::into)?))
+	}
+}
+
+impl<T1, T2, T3, T4, T5, T6, T7> FromFrame for (T1, T2, T3, T4, T5, T6, T7)
+where
+	T1: FromFrame,
+	T2: FromFrame,
+	T3: FromFrame,
+	T4: FromFrame,
+	T5: FromFrame,
+	T6: FromFrame,
+	T7: FromFrame,
+	<T1 as FromFrame>::Error: Into<std::io::Error>,
+	<T2 as FromFrame>::Error: Into<std::io::Error>,
+	<T3 as FromFrame>::Error: Into<std::io::Error>,
+	<T4 as FromFrame>::Error: Into<std::io::Error>,
+	<T5 as FromFrame>::Error: Into<std::io::Error>,
+	<T6 as FromFrame>::Error: Into<std::io::Error>,
+	<T7 as FromFrame>::Error: Into<std::io::Error>,
+{
+	type Error = std::io::Error;
+
+	fn parse_frame(frame: &mut Bytes) -> Result<Self, Self::Error> {
+		Ok((T1::parse_frame(frame).map_err(Into::into)?, T2::parse_frame(frame).map_err(Into::into)?, T3::parse_frame(frame).map_err(Into::into)?, T4::parse_frame(frame).map_err(Into::into)?, T5::parse_frame(frame).map_err(Into::into)?, T6::parse_frame(frame).map_err(Into::into)?, T7::parse_frame(frame).map_err(Into::into)?))
+	}
+}
+
+impl<T1, T2, T3, T4, T5, T6, T7, T8> FromFrame for (T1, T2, T3, T4, T5, T6, T7, T8)
+where
+	T1: FromFrame,
+	T2: FromFrame,
+	T3: FromFrame,
+	T4: FromFrame,
+	T5: FromFrame,
+	T6: FromFrame,
+	T7: FromFrame,
+	T8: FromFrame,
+	<T1 as FromFrame>::Error: Into<std::io::Error>,
+	<T2 as FromFrame>::Error: Into<std::io::Error>,
+	<T3 as FromFrame>::Error: Into<std::io::Error>,
+	<T4 as FromFrame>::Error: Into<std::io::Error>,
+	<T5 as FromFrame>::Error: Into<std::io::Error>,
+	<T6 as FromFrame>::Error: Into<std::io::Error>,
+	<T7 as FromFrame>::Error: Into<std::io::Error>,
+	<T8 as FromFrame>::Error: Into<std::io::Error>,
+{
+	type Error = std::io::Error;
+
+	fn parse_frame(frame: &mut Bytes) -> Result<Self, Self::Error> {
+		Ok((T1::parse_frame(frame).map_err(Into::into)?, T2::parse_frame(frame).map_err(Into::into)?, T3::parse_frame(frame).map_err(Into::into)?, T4::parse_frame(frame).map_err(Into::into)?, T5::parse_frame(frame).map_err(Into::into)?, T6::parse_frame(frame).map_err(Into::into)?, T7::parse_frame(frame).map_err(Into::into)?, T8::parse_frame(frame).map_err(Into::into)?))
+	}
+}
