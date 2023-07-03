c7adfeb7de024ccdead0ea24d6dc72a8400dae9a
Michael Scofield <mscofield0@tutanota.com>
2023-07-03 15:01:45 +0200

    fix bug with decoding and re-limit size to u32

--- src/wire-framed-core/src/codec.rs
+++ src/wire-framed-core/src/codec.rs
@@ -8,7 +8,7 @@ pub type FramedWrite<S> = tokio_util::codec::FramedWrite<S, FrameCodec>;
 /// Codec type for [`Message`] that implements [`tokio_util::codec::Decoder`] and [`tokio_util::codec::Encoder`].
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct FrameCodec {
-    byte_count: Option<u64>,
+    byte_count: Option<u32>,
     data: BytesMut,
 }
 
@@ -36,9 +36,9 @@ impl Encoder<Bytes> for FrameCodec {
     type Error = std::io::Error;
 
     fn encode(&mut self, item: Bytes, dst: &mut BytesMut) -> Result<(), Self::Error> {
-        let byte_count = item.len() as u64;
-        dst.reserve(std::mem::size_of::<u64>() + byte_count as usize);
-        dst.put_u64(byte_count);
+        let byte_count = item.len() as u32;
+        dst.reserve(std::mem::size_of::<u32>() + byte_count as usize);
+        dst.put_u32(byte_count);
         dst.put(item);
 
         Ok(())
@@ -50,27 +50,31 @@ impl Decoder for FrameCodec {
     type Error = std::io::Error;
 
     fn decode(&mut self, src: &mut BytesMut) -> Result<Option<Self::Item>, Self::Error> {
+        // read the initial frame length
         if self.byte_count.is_none() {
-            if src.len() < std::mem::size_of::<u64>() {
+            if src.len() < std::mem::size_of::<u32>() {
                 return Ok(None);
             }
 
-            let byte_count = src.get_u64();
+            let byte_count = src.get_u32();
             self.data.reserve(byte_count as usize);
             self.byte_count = Some(byte_count);
         }
 
+        // read chunk of data
         let byte_count = self.byte_count.unwrap();
-        let remaining_bytes = (byte_count - self.data.len() as u64) as usize;
-        if src.len() < remaining_bytes {
-            self.data.put(src.split_to(remaining_bytes));
-            return Ok(None);
+        let remaining_bytes = (byte_count - self.data.len() as u32) as usize;
+        let at = std::cmp::min(remaining_bytes, src.len());
+        self.data.put(src.split_to(at));
+
+        // if we have read all the data, return the frame
+        if byte_count == self.data.len() as u32 {
+            let frame = self.data.clone().freeze();
+            self.clear();
+            return Ok(Some(frame))
         }
 
-        self.data.put(src.split_to(remaining_bytes));
-
-        let frame = self.data.split_to(self.data.len()).freeze();
-        self.clear();
-        Ok(Some(frame))
+        // otherwise, wait for more data to arrive to finish the frame
+        Ok(None)
     }
 }
